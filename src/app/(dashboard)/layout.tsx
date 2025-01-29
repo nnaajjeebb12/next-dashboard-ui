58a1d71dc6d69789546f627e2d2a07f9dbe43a88
nnaajjeebb12 <najeeblopez12@gmail.com>
2025-01-29 12:16:45 +0800

    Added a (dashboard) added a menu and navbar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+import '/src/app/globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Lama Dev School Management Dashboard',
+	description: 'Next.js School Management System',
+};
+
+export default function DashboardLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<div className="h-screen flex">
+			{/* LEFT */}
+			<div className="w-[14%] md:[8%] lg:w-[16%] xl:w-[14%]  bg-red-200 p-4">
+				<Link
+					href="/"
+					className="flex items-center justify-center lg:justify-start gap-2">
+					<Image src="/logo.png" alt="logo" width={32} height={32} />
+					<span className="hidden lg:block">School</span>
+				</Link>
+			</div>
+			{/* RIGHT */}
+			<div className="w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+				r
+			</div>
+		</div>
+	);
+}
