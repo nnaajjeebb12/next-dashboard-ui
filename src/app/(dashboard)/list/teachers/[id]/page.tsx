e8a935b4a5549c2c87951136a1475ff16b4751d6
nnaajjeebb12 <najeeblopez12@gmail.com>
2025-02-08 21:22:10 +0800

    finished linking student to db

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -166,7 +166,9 @@ const SingleTeacherPage = () => {
 						<Link className="p-3 rounded-md bg-najSkyLight" href="/">
 							Teacher&apos;s Classes
 						</Link>
-						<Link className="p-3 rounded-md bg-najPurpleLight" href="/">
+						<Link
+							className="p-3 rounded-md bg-najPurpleLight"
+							href={`/list/students?teacherId=${'teacher2'}`}>
 							Teacher&apos;s Students
 						</Link>
 						<Link className="p-3 rounded-md bg-najYellowLight" href="/">
