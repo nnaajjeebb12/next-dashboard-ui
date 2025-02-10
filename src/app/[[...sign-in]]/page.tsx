a039bf2c5502b2d45e1c28f7e57ee86f4d33ae4b
nnaajjeebb12 <najeeblopez12@gmail.com>
2025-02-10 23:00:44 +0800

    Created login page

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -2,28 +2,54 @@
 
 import * as Clerk from '@clerk/elements/common';
 import * as SignIn from '@clerk/elements/sign-in';
+import Image from 'next/image';
 
 const LoginPage = () => {
 	return (
-		<div className="h-screen flex items-center justify-center bg-najSkyLight">
+		<div className="h-screen flex items-center justify-center bg-najDepEdLavander rounded-lg">
 			<SignIn.Root>
 				<SignIn.Step
 					name="start"
-					className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2">
-					<h1>Dr. Juan A. Pastor</h1>
-					<h2>Sign in to your account</h2>
-					<Clerk.GlobalError />
-					<Clerk.Field name="identifier">
-						<Clerk.Label>Username</Clerk.Label>
-						<Clerk.Input type="text" required />
-						<Clerk.FieldError />
+					className="bg-najDepEdVis2Blue p-12 rounded-md shadow-2xl flex flex-col gap-2">
+					<div className="flex flex-col items-center justify-center gap-2">
+						<Image
+							src="/edited-DrJuanLogo.png"
+							alt=""
+							width={100}
+							height={100}
+						/>
+					</div>
+					<h1 className="text-xl font-bold flex items-center justify-center gap-2">
+						Dr. Juan A. Pastor
+					</h1>
+					<h2 className="text-sm text-gray-500 flex items-center justify-center">
+						School Management System
+					</h2>
+
+					<Clerk.GlobalError className="text-sm text-red-400" />
+					<Clerk.Field name="identifier" className="flex flex-col gap-2">
+						<Clerk.Label className="text-xs to-gray-500">Username</Clerk.Label>
+						<Clerk.Input
+							type="text"
+							required
+							className="p-2 rounded-md ring-1 ring-gray-300"
+						/>
+						<Clerk.FieldError className="text-xs text-red-700" />
 					</Clerk.Field>
-					<Clerk.Field name="password">
-						<Clerk.Label>password</Clerk.Label>
-						<Clerk.Input type="password" required />
-						<Clerk.FieldError />
+					<Clerk.Field name="password" className="flex flex-col gap-2">
+						<Clerk.Label className="text-xs to-gray-500">password</Clerk.Label>
+						<Clerk.Input
+							type="password"
+							required
+							className="p-2 rounded-md ring-1 ring-gray-300"
+						/>
+						<Clerk.FieldError className="text-xs text-red-700" />
 					</Clerk.Field>
-					<SignIn.Action submit>Sign In</SignIn.Action>
+					<SignIn.Action
+						submit
+						className="bg-najDepEdRoyalBlue text-white  my-1 rounded-md p-[10px]">
+						Sign In
+					</SignIn.Action>
 				</SignIn.Step>
 			</SignIn.Root>
 		</div>
