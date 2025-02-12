598892ce03ecf6265d7f48c3b828c7643a5f7115
nnaajjeebb12 <najeeblopez12@gmail.com>
2025-02-12 21:00:04 +0800

    Linked User cards to db linked count of students chart linked attendance chart to db

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,7 @@
 import Announcements from '@/components/Announcements';
 import AttendanceChart from '@/components/AttendanceChart';
-import CountChart from '@/components/CountChart';
+import AttendanceChartContainer from '@/components/AttendanceChartContainer';
+import CountChartContainer from '@/components/CountChartContainer';
 import EventCalendar from '@/components/EventCalendar';
 import UserCard from '@/components/UserCard';
 
@@ -20,11 +21,11 @@ const AdminPage = () => {
 				<div className="flex gap-4 flex-col lg:flex-row">
 					{/* COUNT CHART */}
 					<div className="w-full lg:w-1/3 h-[450px]">
-						<CountChart />
+						<CountChartContainer />
 					</div>
 					{/* ATTENDANCE CHART */}
 					<div className="w-full lg:w-2/3 h-[450px]">
-						<AttendanceChart />
+						<AttendanceChartContainer />
 					</div>
 				</div>
 				{/* BOTTOM CHARTS */}
