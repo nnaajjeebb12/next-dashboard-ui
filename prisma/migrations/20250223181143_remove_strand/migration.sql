ce93f08d298750422e47ddf78f937268015f2b49
nnaajjeebb12 <najeeblopez12@gmail.com>
2025-02-24 02:13:42 +0800

    Schema_remember_to_add_strand

---
+++ prisma/migrations/20250223181143_remove_strand/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `strand` on the `Class` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Class" DROP COLUMN "strand";
